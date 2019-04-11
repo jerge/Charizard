51b89f48b4d3066a14f2949d6a43b45f3af49d8b
Lambi2000 <lammers@student.chalmers.se>
2019-04-11 11:12:22 +0200

    Added sign up page

---
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/SignUpActivity.java
@@ -0,0 +1,68 @@
+package alexa.projectcharizard.ViewModel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import alexa.projectcharizard.R;
+
+public class SignUpActivity extends Activity {
+
+    private EditText signUpName, signUpPassword,signUpEmail;
+    private Button signUpButton;
+    private TextView alreadySignedUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signUp);
+        setUpUIViews();
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()){
+                    //TODO connect to database and upload
+                }
+            }
+        });
+
+        alreadySignedUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, LogInActivity.class));
+            }
+        });
+
+    }
+
+    private void setUpUIViews(){
+        signUpName = (EditText)findViewById(R.id.signUpName);
+        signUpPassword = (EditText)findViewById(R.id.signUpPassword);
+        signUpEmail = (EditText)findViewById(R.id.signUpEmail);
+        signUpButton = (Button)findViewById(R.id.signUpButton);
+        alreadySignedUp = (TextView)findViewById(R.id.alreadySignedUp);
+    }
+
+    private Boolean validate(){
+
+        Boolean result = false;
+        String name = signUpName.getText().toString();
+        String password = signUpPassword.getText().toString();
+        String email = signUpEmail.getText().toString();
+
+        if (name.isEmpty() && email.isEmpty() && password.isEmpty()){
+            Toast.makeText(this, "Please enter details", Toast.LENGTH_SHORT).show();
+        } else {
+            result = true;
+        }
+
+        return result;
+    }
+
+}
