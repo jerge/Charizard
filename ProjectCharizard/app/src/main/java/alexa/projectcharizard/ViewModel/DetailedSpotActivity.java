4cc885a5b23c397fe71ff421297c2a371c682893
Lambi2000 <lammers@student.chalmers.se>
2019-04-16 16:33:18 +0200

    Added ugly detailed view of spot

---
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/DetailedSpotActivity.java
@@ -0,0 +1,15 @@
+package alexa.projectcharizard.ViewModel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alexa.projectcharizard.R;
+
+public class DetailedSpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_spot);
+    }
+}
