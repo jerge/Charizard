0c24caf0af1cff4cef8eff2dcbc9ceeb0d143473
sedinlovisa <sedinl@student.chalmers.se>
2019-04-17 11:37:25 +0200

    update files

--- ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/frmdemo.java
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/frmdemo.java
@@ -2,13 +2,18 @@ package alexa.projectcharizard.ViewModel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import alexa.projectcharizard.R;
 
 public class frmdemo extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,23 @@ public class frmdemo extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World 2!");
+        myRef.setValue("Hello, World 4!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
