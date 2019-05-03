afed8cb18ed31cfe05774cab8dfac105b300d6e3
Jerge <erikjer@student.chalmers.se>
2019-05-03 11:19:32 +0200

    fix but actually fixed

--- ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/AboutFragment.java
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/AboutFragment.java
@@ -21,14 +21,15 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_about, container, false);
 
-        spotDescription = (TextView)getView().findViewById(R.id.spotDescription);
-        spotName = (TextView) getView().findViewById(R.id.spotName);
+        spotDescription = (TextView)v.findViewById(R.id.spotDescription);
+        spotName = (TextView) v.findViewById(R.id.spotName);
 
         spotDescription.setText(getActivity().getIntent().getStringExtra("SpotDescription"));
         spotName.setText(getActivity().getIntent().getStringExtra("SpotName"));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        return v;
     }
 
 }
