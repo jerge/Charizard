dc4595f9df29bcf3f578cc67f2fdb2fe17a7a72b
Jerge <erikjer@student.chalmers.se>
2019-05-07 10:27:56 +0200

    add filter button

--- ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/MapsActivity.java
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/MapsActivity.java
@@ -76,7 +76,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setPadding(200,0,0,0);
         float initialZoomLevel = initZoom();
         LatLng initialLocation = initLoc();
 
