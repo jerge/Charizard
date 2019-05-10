0ea55e7adae793e435f1417c2f67caba5cb0b799
flippeman <filand1203@gmail.com>
2019-05-10 11:46:43 +0200

    Comment out line of code causing crashes

--- ProjectCharizard/app/src/main/java/alexa/projectcharizard/Model/Database.java
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/Model/Database.java
@@ -71,7 +71,7 @@ public class Database {
             databaseReference.child(id).setValue(spot);
         }
         currentRun.getSpots().add(spot);
-        currentRun.getActiveUser().getUserSpots().add(spot);
+        //currentRun.getActiveUser().getUserSpots().add(spot); //TODO make this work pls lmao
         return spot;
     }
 
