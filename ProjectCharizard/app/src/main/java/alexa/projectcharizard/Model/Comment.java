c87f2b8f4ddf0f5b8f267a17b7ed65633cee9ba6
Alexander Selmanovic <alesel@student.chalmers.se>
2019-05-10 10:34:46 +0200

    Created Comment Class

---
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/Model/Comment.java
@@ -0,0 +1,15 @@
+package alexa.projectcharizard.Model;
+
+public class Comment {
+
+    //private User user;
+    private String comment;
+
+    public Comment (//User user,
+                    String comment){
+        //this.user = user;
+        this.comment = comment;
+    }
+
+
+}
