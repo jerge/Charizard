e1104d1134ea8d91f5c007f13b00b0f628754ce4
flippeman <filand1203@gmail.com>
2019-05-14 11:12:53 +0200

    Change creatorUser to creatorId in Spot.java
    since spots doesn't really need an entire user, only a reference to one

--- ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/SignInActivity.java
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/SignInActivity.java
@@ -119,7 +119,11 @@ public class SignInActivity extends Activity {
         for (User user : currentRun.getUsers()) {     //checks every user in the user list
             if (user.getUsername().equals(usernameInput)) {         //if the username matches
                 if (passwordInput.equals(user.getPassword())) {     //...and the password matches
-                    CurrentRun.getInstance().setActiveUser(user);
+                    currentRun.setActiveUser(user);
+                    System.out.println(user.getUsername());
+                    System.out.println(currentRun);
+                    System.out.println(CurrentRun.getActiveUser());
+                    System.out.println("hej");
                     Intent intent = new Intent(this, MapsActivity.class);
                     startActivity(intent);
                     credErrorText.setVisibility(View.INVISIBLE);
