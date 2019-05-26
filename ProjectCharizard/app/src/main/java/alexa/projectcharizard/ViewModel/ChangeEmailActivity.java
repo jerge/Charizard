f6ca17d233802c4706a6610912a49d21540dd436
Mathias <lammers@student.chalmers.se>
2019-05-26 13:00:33 +0200

    Updated a bunch of cosmetic stuff

--- ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/ChangeEmailActivity.java
+++ ProjectCharizard/app/src/main/java/alexa/projectcharizard/ViewModel/ChangeEmailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -35,6 +36,8 @@ public class ChangeEmailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_change_email);
         initView();
         initText();
